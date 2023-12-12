3bde1ad1f01236ade795d7313cb32e0d67413e19
javadAlip <javadalipp911@gmail.com>
2023-12-13 00:32:29 +0530

    updated

--- fotogram_frontend/src/App.js
+++ fotogram_frontend/src/App.js
@@ -1,2 +1,26 @@
-import React from "react"; 
-importScripts
+import React from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './container/Home';
+import { useEffect } from 'react';
+import { gapi } from 'gapi-script';
+
+const clientId = "973735556425-cdaf3sfi07al7t9ld3ijdgs2st95eu1i.apps.googleusercontent.com"
+const App = () => {
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId: clientId,
+        scope: ""
+      })
+    };
+    gapi.load('client:auth2', start)
+  })
+  return (
+    <Routes>
+      <Route path='login' element={<Login />} />
+      <Route path='/' element={<Home />} />
+    </Routes>
+  );
+};
+export default App;
