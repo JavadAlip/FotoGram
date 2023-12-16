5dff14d8c5f308687938dc271610cd3528a27958
javadAlip <javadalipp911@gmail.com>
2023-12-16 21:55:09 +0530

    react-icons

--- fotogram_frontend/src/container/Pins.jsx
+++ fotogram_frontend/src/container/Pins.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Navbar, Feed, Search, CreatePin, PinDetail } from '../components'; 
-
+import Search from '../components/Search.jsx';
+import Navbar from '../components/Navbar.jsx';
+import Feed from '../components/Feed.jsx';
+import CreatePin from '../components/CreatePin.jsx';
+import PinDetail from '../components/PinDetail.jsx';
 const Pins = ({ user }) => {
   const [searchTerm, setSearchTerm] = useState('');
 
@@ -9,16 +12,16 @@ const Pins = ({ user }) => {
     <div className='px-2 md:px-5'>
       <div className='bg-gray-50'>
         <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm}  user={user}/>
+        </div>
         <div className='h-full'>
           <Routes>
-            <Route path='/' element={<Feed />} />
-            <Route path='/category/:categoryId' element={<Feed />} />
-            <Route path='/pin-detail/1' element={<PinDetail user={user} />} /> 
+            <Route path='/' element={< Feed />} />
+            <Route path='/category/:categoryId' element={< Feed />} />
+            <Route path='/pin-detail/:pinId' element={<PinDetail user={user} />} /> 
             <Route path='/create-pin' element={<CreatePin user={user} />} />
             <Route path='/search' element={<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />} />
           </Routes>
         </div>
-      </div>
     </div>
   );
 }
